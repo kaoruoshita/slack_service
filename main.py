ac2b03e3f68713a0957ad5dd5e06ee0346f73aac
Kaoru Oshita <kaoru.oshita@Kaorus-MacBook-Pro.local>
2017-02-28 16:46:12 +0900

    let's handle event.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from slackclient import SlackClient
 import json
 
-def _send_message(channel_id, message):
+def _send_message(api_key, channel_id, message):
+    slack_client = SlackClient(api_key)
     res = slack_client.api_call(
         "chat.postMessage",
         channel=channel_id,
@@ -10,8 +11,23 @@ def _send_message(channel_id, message):
         # icon_emoji=':robot_face:'
        # as_user = True
     )
+    return res
+
+def _format_options(options):
+  my_dict = {}
+  if options:
+    for option in options:
+        my_dict[option['key']] = option['val']
+
+  return my_dict
+
+
 
 def handle(event, context):
-    _send_message(channel_id, message)
+    data = _format_options(event.get("options", None))
+    message = data.get("Message")
+    api_key = data.get("API Key")
+    channel_id = data.get("Channel ID")
+    res = _send_message(api_key, channel_id, message)
 
-handle(1,1)
+    return res
